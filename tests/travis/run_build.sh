fcfaee87cb4b594b0a30f85e46dc6b6f17fdebc0
Mark Guinn <mark@adaircreative.com>
2015-07-04 20:52:58 +0100

    First crack at integrating with travis

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
 #if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-if [ -n "$COVERAGE" ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split$COVERAGE --coverage-clover ~/builds/ss/shop/coverage.xml
-else
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist
-fi
